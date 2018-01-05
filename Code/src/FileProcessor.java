082b5695420d38f5dc9c27e4d817dbb50b8660cb
alchemist009 <alchemist009@gmail.com>
2018-01-05 23:59:57 +0530

    Hack

---
+++ Code/src/FileProcessor.java
@@ -0,0 +1,31 @@
+import jdk.nashorn.internal.runtime.regexp.joni.Regex;
+
+import javax.swing.*;
+import java.io.*;
+import java.util.Scanner;
+
+public class FileProcessor {
+
+
+    public static void main(String[] args) throws IOException {
+
+        File inputFile = new File("cities.txt");
+        File tempFile = new File("newCities.txt");
+
+        BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+        BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile));
+
+        String st;
+        while((st = reader.readLine()) != null){
+            st= st.replaceAll("[0-9]","\r\n");
+            writer.write(st);
+            //writer.write("\n");
+        }
+
+        writer.close();
+        reader.close();
+
+        //System.out.println(writer);
+    }
+
+}
